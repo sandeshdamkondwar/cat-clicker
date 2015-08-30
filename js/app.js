eba81351f9d3ee1b2d9e8f4dd7599551275db332
Sandesh Damkondwar <sandeshdamkondwar@gmail.com>
2015-08-31 02:02:04 +0530

    Loading cats from JSON

--- js/app.js
+++ js/app.js
@@ -1,7 +1,53 @@
-var clickCount = 0;
+// Polyfill for isInteger
+Number.isInteger = Number.isInteger || function(value) {
+    return typeof value === "number" &&
+           isFinite(value) &&
+           Math.floor(value) === value;
+};
+
+var cats = [{
+    name: "Killo",
+    image: "cat.jpg",
+    clicks: 21
+}, {
+    name: "Megga",
+    image: "cat.jpg",
+    clicks: 34
+}, {
+    name: "Terra",
+    image: "cat.jpg",
+    clicks: 100
+}, {
+    name: "Zetta",
+    image: "cat.jpg",
+    clicks: 24
+}];
+
+function renderCats (cats) {
+    var catResourcesDir = "img/cats/";
+    var catsContainerTmpl = "";
+
+    $.each(cats, function(index, cat) {
+        var catTemplate = _.template($("#catTemplate").html());
+        catsContainerTmpl += catTemplate(_.extend(cat, {
+            "catResourcesDir": catResourcesDir
+        }));
+    });
+
+    $("#cats").html(catsContainerTmpl);
+}
+
+renderCats(cats);
 
 $catImageContainer = $('.cat-click-container');
 
-$catImageContainer.find('img').on('click', function() {
-    $catImageContainer.find('.click-count').html(++clickCount);
+$catImageContainer.on('click', 'img', function() {
+    var $cat = $(this).closest(".cat");
+    var $counter = $cat.find(".click-count");
+    var previousCount = parseInt($.trim($counter.html()));
+    if (Number.isInteger(previousCount)) {
+        $counter.html(++previousCount);
+    } else {
+        // TODO: Show notification explaining problem
+    }
 });
